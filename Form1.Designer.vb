30e35c1d497170f3a706d3a1bb0d2d40f5e3fdd9
GENESIS ANAHI MEJIA DIAZ <124095475+GenesisMejia@users.noreply.github.com>
2023-05-15 22:56:28 -0600

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtId = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 51)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(142, 39)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Consumir api Json"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txtId
+        '
+        Me.txtId.Location = New System.Drawing.Point(92, 12)
+        Me.txtId.Name = "txtId"
+        Me.txtId.Size = New System.Drawing.Size(100, 20)
+        Me.txtId.TabIndex = 1
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(160, 51)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(142, 39)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Consumir api (Parametro)"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(310, 100)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.txtId)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents txtId As TextBox
+    Friend WithEvents Button2 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,55 @@
+﻿Imports Newtonsoft.Json
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim api = New DBApi()
+
+        Dim persona = New Persona()
+        persona.Nombre = "Saulo de Tarso"
+        persona.Oficio = "Predicador"
+
+        Dim url = "https://jsonplaceholder.typicode.com/users"
+
+        Dim headers = New List(Of Parametro) From {
+            New Parametro("Authorization", "bearer asdasdas"),
+            New Parametro("Cookie", "asdasdasdas")
+        }
+
+        Dim parametros = New List(Of Parametro)
+
+        Dim response = api.Post(url, headers, parametros, persona)
+
+        Dim result = JsonConvert.DeserializeObject(Of respuesta)(response)
+
+        txtId.Text = result.id.ToString()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim api = New DBApi()
+
+        Dim url = "https://jsonplaceholder.typicode.com/users"
+
+        Dim headers = New List(Of Parametro) From {
+            New Parametro("Authorization", "bearer asdasdas"),
+            New Parametro("Cookie", "asdasdasdas")
+        }
+
+        Dim parametros = New List(Of Parametro) From {
+            New Parametro("Nombre", "Saulo de Tarso"),
+            New Parametro("Oficio", "Predicador")
+        }
+
+        Dim response = api.Post(url, headers, parametros, Nothing)
+
+        Dim result = JsonConvert.DeserializeObject(Of respuesta)(response)
+
+        txtId.Text = result.Nombre.ToString()
+    End Sub
+End Class
+
+Class respuesta
+    Public Property Nombre As String
+    Public Property Oficio As String
+    Public Property id As Integer
+End Class
